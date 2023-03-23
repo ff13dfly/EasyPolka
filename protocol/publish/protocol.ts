fe6dc521c23d228aced2eefaa3264d7c2575fc5a
fuu <fu@yihaofang.com>
2023-03-23 10:44:17 +0800

    TS type keyword test.

--- protocol/publish/protocol.ts
+++ protocol/publish/protocol.ts
@@ -1,21 +1,69 @@
 //!important This is the library for Esay Protocol
 
-var hello : string = "Hello World!"
-console.log(hello);
+// import anchorJS, need to confirm and get libs
+import {anchorJS} from '../lib/anchor.js';
 
-var sites:string[]; 
-sites = ["Google","Runoob","Taobao"]
+// npx tsc protocol.ts
+const proto={
+    "data":{
+        "fmt":"string",
+        "code":"string",
+    },
+    "app":{
 
+        "lib":"localtion",
+        "ver":"version",
+    }
+};
 
-console.log(sites);
+const check={
+    auto:(proto:object)=>{
 
-var Preter:any ={
-    run: function(anchor:string,ck:Function){
-        console.log(anchor);
-        return ck && ck(true);
+    },
+    string:(str:string)=>{
+
+    },
+    version:(str:string)=>{
+
     },
 };
 
-Preter.run(hello,function(res:boolean){
-    console.log(res);
-});
+type cAppObject={
+
+}
+type dataObject={
+    
+}
+
+type anchorObject={
+    "name":string,
+    "protocol":object,
+    "raw":object|string,
+    "block":number,
+    "stamp":number,
+    "pre":number,
+    "signer":string,
+    "empty":boolean,
+    "owner":string,
+    "sell":boolean,
+    "cost":number,
+    "target":string,
+};
+
+const funs={
+    //check
+    init:(name:string,ck:any)=>{
+        anchorJS.search(name,(data:anchorObject)=>{
+            console.log(data);
+        });
+    },
+
+    //decode the cApp data
+    decode:(protocol:object,raw:string|object)=>{
+
+    },
+}
+
+export default {
+	init:funs.init,
+};
