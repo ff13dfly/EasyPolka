92287404ffafd97c949e2a47e24de5116be8d20d
fuu <fu@yihaofang.com>
2023-03-27 20:36:37 +0800

    easy protocol call part update

--- protocol/src/protocol.ts
+++ protocol/src/protocol.ts
@@ -7,8 +7,8 @@
 //anchor location object
 export type localtionObject=[
     name  : string,
-    block : number,
-];
+    block? : number,
+] | string;
 
 //anchor data object, input from anchor.js
 export type anchorObject={
@@ -37,26 +37,35 @@ export type errorObject={
 
 //data type object
 export type dataObject={
-    "type":string;
-    "format":string;
-    "code":string;
+    "type":string;              //enum, ["data","app"]
+    "fmt":string;
+    "code":string;              //data code
+    "call"?:anchorObject;       //call target anchor
+    "auth"?:anchorObject;       //anchor which the auth list storaged.
 }
 
 //cApp type object
 export type cAppObject={
     "type":string;
-    "format":string;
-    "lib":localtionObject[]|string[];
+    "fmt":string;
+    "lib"?:localtionObject[]|string[];
+    "ver"?:string;
+    "auth"?:anchorObject;
 }
+
+//auth format
+// {
+//     "SS58 address":"{expired_block_number}"   //start_block is the storage one.
+// }
 
 //input API object type, define the 
 export type APIObject={
-    "common":{
+    "common":{              //normal anchor function
         "target":(anchor:string,block:number,ck:Function) => anchorObject|errorObject;
         "latest":(anchor:string,ck:Function) => anchorObject|errorObject;
         "history"?:(anchor:string,ck:Function) => anchorObject[]|errorObject;
     };
-    "auto"?:Function;
+    "auto"?:Function;       //Gateway auto access
 }
 
 /********************************/
@@ -67,9 +76,10 @@ export type APIObject={
 export type cAppResult={
     API:APIObject;
     error:errorObject[];
+    app:Function|null;
     parameter:string[];
-    from:anchorObject|null;
-    back?:string[];
+    from?:anchorObject;     //if the cApp is called
+    back?:string[];         //parameter when callback the from cApp
 }
 
 //default value object
