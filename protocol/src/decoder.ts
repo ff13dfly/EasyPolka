7aea2f61e1adee4ad02188348bb91792690da1b4
fuu <fu@yihaofang.com>
2023-04-02 08:06:24 +0800

    linker deoder definition update

--- protocol/src/decoder.ts
+++ protocol/src/decoder.ts
@@ -1,7 +1,28 @@
 //!important This is the library for decoding anchor link
 
-const decoder=(link:string,ck:Function)=>{
-    const res={};
+import { anchorLocation}from "./protocol";
+
+type decoderResult={
+    location:anchorLocation;
+    param?:params;
+};
+
+interface params {
+    [anchor: string]: any;
+}
+
+const setting={
+    "check":false,      //auto check the anchor exsist
+    "utf8":true,        //auto decode parameter to UTF8
+};
+
+
+const decoder=(link:string,cfg?:any,ck?:Function)=>{
+    let res:decoderResult={
+        location:["hello",223],
+
+    };
+
     return ck && ck(res);
 };
 
