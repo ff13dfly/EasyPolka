32e751cbf4739dcd4a66f8237bd2318f1909da3b
fuu <fu@yihaofang.com>
2023-05-04 21:54:42 +0800

    node args test.

---
+++ protocol/test/node_parameter.js
@@ -0,0 +1,10 @@
+//node node_parameter.js one two=three four
+
+//node node_parameter.js anchor://full_app/
+
+var args = process.argv.slice(2);
+console.log(args);
+
+// process.argv.forEach(function (val, index) {
+//     console.log(index + ': ' + val);
+// });
