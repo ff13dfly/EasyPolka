98b045921b63202007ce410f9f2d7633dea09185
fuu <fu@yihaofang.com>
2023-06-08 22:41:19 +0800

    valid struct added

---
+++ gateway/lib/valid.js
@@ -0,0 +1,26 @@
+const self={
+    number:(param,isArray,max)=>{
+        if(isArray){
+        
+        }
+    },
+    string:(param,isArray,max)=>{
+
+    },
+    SS58:(param,isArray)=>{
+
+    },
+    anchor:(param,isArray)=>{
+
+    },
+    u32:(param,isArray)=>{
+
+    },
+
+    //check the type of definition
+    isArray:(str)=>{
+        const result={type:"",isArray:false};
+    },
+}
+
+module.exports=self;
