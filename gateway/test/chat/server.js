2c3e7f73f736e2434600856343987b807751143d
fuu <fu@yihaofang.com>
2023-09-25 09:01:45 +0800

    OS chat demo done.

--- gateway/test/chat/server.js
+++ gateway/test/chat/server.js
@@ -10,41 +10,56 @@ const tools = require("../ws/lib/tools");
 console.log(`[ WS url ] http://localhost:${port}`);
 
 const clients={};
+const amap={},bmap={};
+
+const self={
+    send:(obj,spam,order)=>{
+        if(!clients[spam]) return false;
+        obj.order=order;
+        clients[spam].send(JSON.stringify(obj));
+    },
+    success:(spam,order)=>{
+        self.send({status:1},spam,order);
+    },
+    failed:(spam,order)=>{
+        self.send({status:0},spam,order);
+    },
+};
 wss.on('connection', function connection(ws,request, client) {
     const uid=tools.char(12);
     clients[uid]=ws;
 
-    ws.send(JSON.stringify({"spam":uid}));
+    ws.send(JSON.stringify({"spam":uid,"act":"init"}));
     ws.on('error', console.error);
     ws.on('message', function message(res){
         const str=res.toString();
         console.log(str);
-        // const str=res.toString();
-        // const input=JSON.parse(str);
-        // const active=clients[input.spam];
-        // console.log(`Request[${input.index}]:${tools.stamp()}`)
-        // try {
-        //     const cfg={
-        //         method: 'post',
-        //         url: "http://localhost:4444",
-        //         data:{"index":input.index},
-        //     }
-        //     axios(cfg).then((res)=>{
-        //         const data=res.data;
-        //         data.stamp=tools.stamp();
-        //         console.log(`Response[${data.index}]:${tools.stamp()}`);
-        //         active.send(JSON.stringify(data));
-        //     }).catch((err)=>{
-        //        console.log(err);
-        //     });
+        if(!str) return false;
 
-        // } catch (error) {
-        //     console.log(error);
-        //     console.log("not json data");
-        // }
+        try {
+            const input=JSON.parse(str);
+            switch (input.act) {
+                case "reg":
+                    amap[input.acc]=input.spam;
+                    bmap[input.spam]=input.acc;
+                    self.success(input.spam,input.order);
+                    break;
+                case "chat":
+                    const to=input.to;
+                    console.log(amap[to]);
+                    if(!clients[amap[to]]){
+                        return  self.failed(input.spam,input.order);
+                    }
+                    self.send({act:"chat",msg:input.msg,from:bmap[input.spam]},amap[to],input.order);
+                    self.success(input.spam,input.order);
 
-        // setInterval(()=>{
-
-        // },2000)
+                    break;
+                default:
+                    break;
+            }
+            
+        } catch (error) {
+            
+        }
     });
 });
