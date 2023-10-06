04d8a10a2bacd642417fa0aebc34f2a0e6c936fa
fuu <fu@yihaofang.com>
2023-10-06 09:22:47 +0800

    AES demo done.

--- gateway/test/aes/server.js
+++ gateway/test/aes/server.js
@@ -2,34 +2,70 @@ const {WebSocketServer} =require('ws');
 const port=5566;
 const wss = new WebSocketServer({port: port});
 const tools = require("../ws/lib/tools");
+const encry = require("../../lib/encry");
+
 console.log(`[ WS url ] http://localhost:${port}`);
 
+
+let ws=null;
 const self={
-    send:(obj,spam,order)=>{
-        if(!clients[spam]) return false;
-        obj.order=order;
-        clients[spam].send(JSON.stringify(obj));
+    send:(obj)=>{
+        //console.log(obj);
+        ws.send(JSON.stringify(obj));
     },
-    success:(spam,order)=>{
-        self.send({status:1},spam,order);
+    success:(obj)=>{
+        obj.success=true;
+        self.send(obj);
     },
-    failed:(spam,order)=>{
-        self.send({status:0},spam,order);
+    failed:(msg)=>{
+        self.send({error:msg});
     },
 };
 
-wss.on('connection', function connection(ws,request, client) {
-
-    //ws.send(JSON.stringify({"spam":uid,"act":"init"}));
+wss.on('connection', function connection(active,request, client) {
+    ws=active;
     ws.on('error', console.error);
     ws.on('message', function message(res){
         const str=res.toString();
-        console.log(str);
+        //console.log(str);
         if(!str) return false;
 
         try {
             const input=JSON.parse(str);
-            
+            switch (input.act) {
+
+                case "encode":
+                    const pass=input.pass;
+                    const ctx=input.ctx;
+                    
+                    //1. md5 password to encry the data
+                    //password abc
+                    //900150983cd24fb0
+                    //d6963f7d28e17f72
+                    const md5=encry.md5(pass);
+                    encry.setKey(md5.substring(0,16));
+                    encry.setIV(md5.substring(16,32));
+                    const code=encry.encrypt(ctx);
+
+                    //2. set default IV
+                    //encry.setKey(md5);
+                    //encry.setIV("aaaaaa");
+                    //const code=encry.encrypt(ctx);
+
+                    const de=encry.decrypt(code);
+
+                    self.success({"md5":md5,"code":code,"circle":de});
+                    break;
+
+                case "decode":
+                    const depass=input.pass;
+                    const dectx=input.ctx;
+
+                    break;
+
+                default:
+                    break;
+            }
             
         } catch (error) {
             
