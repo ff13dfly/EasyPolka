bc6f884151694ca205e564433b6af24d73a1c4bf
fuu <fu@yihaofang.com>
2023-10-07 21:11:05 +0800

    Chat UI improvement

--- OS/src/components/contact_list.js
+++ OS/src/components/contact_list.js
@@ -1,4 +1,4 @@
-import { Row, Col,Image } from 'react-bootstrap';
+import { Row, Col, Image } from 'react-bootstrap';
 import { useState, useEffect } from 'react';
 
 import Chat from './chat';
@@ -9,18 +9,18 @@ import tools from '../lib/tools';
 function ContactList(props) {
 
   const size = [12];
-  const dv={xs:3,sm:3,md:3,lg:3,xl:6,xxl:6};
+  const dv = { xs: 3, sm: 3, md: 3, lg: 3, xl: 6, xxl: 6 };
   const funs = props.funs;
-  const count=props.count;
+  const count = props.count;
 
   let [contact, setContact] = useState({});
   let [select, setSelect] = useState({});
 
-  const self={
-    click:(address,ev)=>{
-      funs.dialog.show(<Chat address={address} />,"Chatting");
+  const self = {
+    click: (address, ev) => {
+      funs.dialog.show(<Chat address={address} />, "Chatting");
     },
-    select:(address)=>{
+    select: (address) => {
       select[address] = !select[address];
       props.fresh();
       props.select(select);
@@ -37,23 +37,26 @@ function ContactList(props) {
   return (
     <Row index={count}>
       {Object.keys(contact).map((address, index) => (
-        <Col xs={dv.xs} sm={dv.sm} md={dv.md} lg={dv.lg} xl={dv.xl} xxl={dv.xxl} key={index} onClick={(ev)=>{
-          props.edit?self.select(address):self.click(address,ev);
+        <Col xs={dv.xs} sm={dv.sm} md={dv.md} lg={dv.lg} xl={dv.xl} xxl={dv.xxl} key={index} onClick={(ev) => {
+          props.edit ? self.select(address) : self.click(address, ev);
         }}>
           <Row>
             <Col xs={size[0]} sm={size[0]} md={size[0]} lg={size[0]} xl={size[0]} xxl={size[0]}
               className="pt-2">
-                <Image 
-                    src={`https://robohash.org/${address}.png`}
-                    rounded
-                    width="100%"
-                  />
-              <small>{address.length>10?tools.shorten(address,4):address}</small><br/>
-              <small><input hidden={!props.edit} type="checkbox"  
-                checked={!select[address]?false:select[address]}
-                onChange={(ev)=>{
+              <Image
+                src={`https://robohash.org/${address}.png`}
+                rounded
+                width="100%"
+                style={{minHeight:"80px"}}
+              />
+              <span className='count'>3</span>
+              <small>{address.length > 10 ? tools.shorten(address, 4) : address}</small><br />
+              <small><input hidden={!props.edit} type="checkbox"
+                checked={!select[address] ? false : select[address]}
+                onChange={(ev) => {
                   //self.change(ev,address);
-                }} style={{marginRight:"5px"}}/>@{contact[address].network}</small>
+                }} style={{ marginRight: "5px" }} />@{contact[address].network}</small>
+
             </Col>
           </Row>
         </Col>
--- OS/src/data/contacts.js
+++ OS/src/data/contacts.js
@@ -1,17 +1,23 @@
 module.exports={
-  "5CSTSUDaBdmET2n6ju9mmpEKwFVqaFtmB8YdB23GMYCJSgmw":{
+  "5FQmGPk7qGBmU3K6kDfMSBiUHBYq5NqXpx93KFEvDvyz5sRJ":{
     "intro":"Officail test A",
     "status":"",
     "type":"friend",
     "network":"Anchor",
   },
-  "5EUFM4U2LUypvctxvqnEV2vb3hBABspWUYEjErwRpTbsYEaJ":{
+  "5CSTSUDaBdmET2n6ju9mmpEKwFVqaFtmB8YdB23GMYCJSgmw":{
     "intro":"Officail test B",
     "status":"",
     "type":"friend",
     "network":"Anchor",
   },
-  "5DtBERu7U1SwPD1iebf5zHgjRsUnrU3iQgswySXeu6PK7eL4":{
+  "5FHneW46xGXgs5mUiveU4sbTyGBzmstUspZC92UhjJM694ty":{
+    "intro":"Officail test C",
+    "status":"",
+    "type":"friend",
+    "network":"Anchor",
+  },
+  "5FLSigC9HGRKVhB9FiEo4Y3koPsNmBmLJbpXg2mp1hXcS59Y":{
     "intro":"Officail test C",
     "status":"",
     "type":"friend",
